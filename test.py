534bab3e6494535b00dfe1fc8158815738b05e8f
aleksac99 <aleksa.cvetanovic99@gmail.com>
2023-12-28 17:50:37 +0100

    Add episode recording

--- test.py
+++ test.py
@@ -1,15 +1,13 @@
 import torch
 import gymnasium as gym
+from gymnasium.wrappers.record_video import RecordVideo
 from dqn.wrapper import wrap
 from dqn.model import DQN
 from dqn.agent import DQNAgent
-from dqn.trainer import Trainer
-from torch.optim import Adam
-from torch.nn import MSELoss
 
-
-env = env = gym.make("ALE/Pong-v5", obs_type='grayscale', frameskip=4, render_mode='human')
+env = env = gym.make("ALE/Pong-v5", obs_type='grayscale', frameskip=4, render_mode='rgb_array')
 env = wrap(env)
+env = RecordVideo(env, 'out_pong')
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
 
@@ -18,7 +16,7 @@ state, _ = env.reset()
 
 state
 dqn = DQN(state.shape, env.action_space.n)
-dqn.load_state_dict(torch.load('dqn_state_dict.pt', map_location=device))
+dqn.load_state_dict(torch.load('out_pong/dqn_state_dict_20.pt', map_location=device))
 
 agent = DQNAgent(
     dqn,
@@ -27,9 +25,9 @@ agent = DQNAgent(
 
 while not done:
 
-    a = agent.get_action(state.unsqueeze(0), 'eps_greedy', env.action_space, 0)
+    a = agent.get_action(state.unsqueeze(0), 'greedy', env.action_space, 0)
     state, reward, terminated, truncated, info = env.step(a)
     done = terminated or truncated
     env.render()
 
-env.close()
+#env.close()
